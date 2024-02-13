0be9012e40d597a689abc5ca054765197f5f2439
Anatolii Ivanov <gotnickname@gmail.com>
2024-02-13 21:58:06 +0530

    wired up docker compose startup project

--- Personnel_Records.DAL/AppDbContext.cs
+++ Personnel_Records.DAL/AppDbContext.cs
@@ -14,7 +14,6 @@ public class AppDbContext : DbContext
 
 	public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 	{
-		Database.EnsureCreated();
 	}
 
 	protected override void OnModelCreating(ModelBuilder modelBuilder)
